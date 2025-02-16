44e978f1c5d5b623257b66f90797f6e44f6e5f1e
Carlos Muñoz <chavo16@hotmail.com>
2025-02-16 16:12:57 -0500

    add suggester components for settings tab
    Inspiration and code taken from Templater plugin.

---
+++ suggesters/FileSuggester.ts
@@ -0,0 +1,42 @@
+// Credit for this goes to SilentVoid13's templater plugin suggesters:
+// https://github.com/SilentVoid13/Templater/blob/master/src/settings/suggesters/FileSuggester.ts
+
+import { Plugin, TAbstractFile, TFile } from "obsidian";
+import { TextInputSuggest } from "./suggest";
+import TemplaterPlugin from "main";
+
+export enum FileSuggestMode {
+    TemplateFiles,
+    ScriptFiles,
+}
+
+export class TemplateFileSuggest extends TextInputSuggest<TFile> {
+    constructor(
+        public inputEl: HTMLInputElement,
+        private plugin: Plugin,
+        private rootFolder: string
+    ) {
+        super(plugin.app, inputEl);
+    }
+
+    getSuggestions(input_str: string): TFile[] {
+        let templateFiles =
+            this.app.vault.getFolderByPath(this.rootFolder)?.children
+                .filter(f => f instanceof TFile)
+                .filter(f => f.extension === "md")
+                .filter(f => f.path.toLowerCase().contains(input_str.toLowerCase()));
+                //.map(f => f.path)
+
+        return templateFiles ? templateFiles.slice(0, 1000) : [];
+    }
+
+    renderSuggestion(file: TFile, el: HTMLElement): void {
+        el.setText(file.path);
+    }
+
+    selectSuggestion(file: TFile): void {
+        this.inputEl.value = file.path;
+        this.inputEl.trigger("input");
+        this.close();
+    }
+}
